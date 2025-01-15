a1742961c794d525ca3ac68619c5132235691ae4
Doğan Can Bakır <dogancanbakir@protonmail.com>
2025-01-15 20:35:19 +0300

    add philosophy page

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -27,6 +27,12 @@ export default async function sitemap() {
       priority: 1,
     },
     {
+      url: `${SITE.url}/philosophy`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.9,
+    },
+    {
       url: `${SITE.url}/manifesto`,
       lastModified: new Date(),
       changeFrequency: "yearly",
